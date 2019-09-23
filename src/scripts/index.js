3884077d6440216ed78a3e806f890c633e469a91
diogoperes <diogo.peres@hotmail.com>
2019-09-23 21:40:02 +0100

    images random and arrange pieces created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
 import '../styles/index.scss';
 // import $ from 'jquery';
+import attachFastClick  from 'fastclick';
 
 console.log('webpack starterkit');
 
 
 var zIndex=1;
-var host='http://www.wdisseny.com/puzzle/';
+// var host='http://www.wdisseny.com/puzzle/';
+var host='http://picsum.photos/';
 let model;
 let numRows;
 let numCols;
@@ -15,7 +17,11 @@ let pecaAlSeuLloc;
 let timeInitial;
 let timeEnd;
 
-let sizeOfPieces = 60;
+//double click variables
+let clickCount = 0;
+let singleClickTimer;
+
+let sizeOfPieces = 100;
 
 let pezaSeleccionada= false;
 
@@ -167,7 +173,12 @@ function createSvg(piecePath,x,y,index){
   let xmlns = "http://www.w3.org/2000/svg";
   let svg=document.createElementNS(xmlns,"svg");
   let idPattern="row"+x+"col"+y;
-  let pattern='<defs><pattern id="'+ idPattern +'" patternUnits="userSpaceOnUse" width="'+(numCols*sizeOfPieces)+'" height="'+(numRows*sizeOfPieces)+'"><image href="'+host+model.urlImg+'" x="'+(x*-100+40)+'" y="'+(y*-100+40)+'"  /></pattern></defs>';
+  // let pattern='<defs><pattern id="'+ idPattern +'" patternUnits="userSpaceOnUse" width="'+(numCols*sizeOfPieces)+'" height="'+(numRows*sizeOfPieces)+'"><image href="'+host+model.urlImg+'" x="'+(x*-100+40)+'" y="'+(y*-100+40)+'"  /></pattern></defs>';
+  let pattern= `<defs>
+    <pattern id="${idPattern}" patternUnits="userSpaceOnUse" width="${numCols*sizeOfPieces}" height="${numRows*sizeOfPieces}">
+      <image href="${host}/${model.width}/${model.height}" x="${x*-100+40}" y="${y*-100+40}"></image>
+    </pattern>
+  </defs>`;
   svg.innerHTML= pattern;
   svg.setAttribute("width",sizeOfPieces * 180 / 100);
   svg.setAttribute("height",sizeOfPieces * 180 / 100);
@@ -183,15 +194,17 @@ function createSvg(piecePath,x,y,index){
   move.style.width = sizeOfPieces + "px";
   move.style.height = sizeOfPieces + "px";
   move.path=path;
-  move.onmousemove=getPos;
-  move.touchstart=getPos;
+  // move.onmousemove=getPos;
+  move.onmousedown=getPos;
+  move.onclick=onDblClick;
+  move.ontouchstart=getPos;
   move.onmouseout=stopTracking;  move.touchcancel=stopTracking;
   //move.setAttribute("classangle","g0")
   move.angle=0;
   move.occupy= false;
   move.position=function(){return {left:this.offsetLeft+(sizeOfPieces/2),top:this.offsetTop +(sizeOfPieces/2)};};
   move.onmouseup=dropPiece;
-  move.touchend=dropPiece;
+  move.ontouchend=dropPiece;
   move.index=index;
   //position indicates the position on the board
   let position = document.createElement("DIV");
@@ -210,42 +223,53 @@ function createSvg(piecePath,x,y,index){
 var i, element;
 var models=[
 
-  {
-    numRows : 4,
-    numCols : 6,
-    urlImg :"4x6.jpg"
-  },
+  // {
+  //   numRows : 4,
+  //   numCols : 6,
+  //   urlImg :"4x6.jpg"
+  // },
   {
     numRows : 2,
     numCols : 2,
-    urlImg :"2x2.jpg"
+    urlImg :"2x2.jpg",
+    width: 200,
+    height: 200,
   },
   {
     numRows : 3,
     numCols : 3,
-    urlImg :"3x3.png"
+    urlImg :"3x3.png",
+    width: 300,
+    height: 300,
   },
   {
     numRows : 4,
     numCols : 4,
-    urlImg :"4x4.jpg"
+    urlImg :"4x4.jpg",
+    width: 400,
+    height: 400,
   },
-
 
   {
     numRows : 5,
     numCols : 5,
-    urlImg :"5x5.png"
+    urlImg :"5x5.png",
+    width: 500,
+    height: 500,
   },
   {
     numRows : 5,
     numCols : 7,
-    urlImg :"7x5.jpg"
+    urlImg :"7x5.jpg",
+    width: 700,
+    height: 500,
   },
   {
     numRows : 6,
     numCols : 8,
-    urlImg :"8x6.jpg"
+    urlImg :"8x6.jpg",
+    width: 800,
+    height: 600,
   },
 
 ];
@@ -256,7 +280,8 @@ function selectModel(i){
   createPuzzle(model);
   document.querySelector("#start").style.height="";
   document.querySelector("#modelsButton").style.height="";
-  document.querySelector("#tap").style.opacity=0;
+  // document.querySelector("#tap").style.opacity=0;
+  document.querySelector("#tap").style.display='none';
 }
 
 function createListModels(){
@@ -267,12 +292,13 @@ function createListModels(){
 
   for (let i=0; i < models.length; i++) {
     let e = models[i];
+    e.imageUrl = `${host}/${e.width}/${e.height}`;
 
     let div = document.createElement('div');
     div.innerHTML = `
         <b>${(i+1)}</b><br>
         ${e.numCols * e.numRows} pieces<br>
-        <img height="${(e.numRows/3*100)}" src="${host+e.urlImg}" width="${e.numcols/3*100}"><br>
+        <img height="${(e.numRows/3*100)}" src="${e.imageUrl}" width="${e.numcols/3*100}"><br>
         <small>${e.numCols*100} x ${e.numRows*100}</small>
       `;
     div.addEventListener("click", () => selectModel(i));
@@ -291,6 +317,20 @@ function createPuzzle(model){
   pezaSeleccionada= false;
   numRows=model.numRows;
   numCols=model.numCols;
+  //adjust piece size to not overflow screen width
+  if(window.innerWidth < numCols * sizeOfPieces + sizeOfPieces) {
+    sizeOfPieces = window.innerWidth / numCols - 1;
+  }
+  //adjust piece size so puzzle height don't overpass 2.2 of screen height
+  if(window.innerHeight / 2.2 < numRows * sizeOfPieces) {
+    sizeOfPieces = (window.innerHeight / 2.2) / numRows;
+  }
+
+  if(sizeOfPieces < 40) {
+    sizeOfPieces = 40;
+    alert('Sorry :/ Your device is too small for this puzzle');
+  }
+
   document.getElementById("container").style.width=numCols*sizeOfPieces +"px";
   document.getElementById("container").style.backgroundPositionY=sizeOfPieces*model.numRows+"px";
   for (let row=0; row<numRows; row++){
@@ -304,18 +344,19 @@ function createPuzzle(model){
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  model=models[2];
+  model=models[1];
   createPuzzle(model);
   createListModels();
   document.getElementById("seeModel").onmouseover=function(){
-    document.getElementById("container").style.backgroundImage="url("+host+model.urlImg+")";
+    document.getElementById("container").style.backgroundImage="url("+model.imageUrl+")";
     document.getElementById("container").style.backgroundPositionY=0;
   };
   document.getElementById( "seeModel").onmouseout=function(){
     document.getElementById("container").style.backgroundPositionY=100*model.numRows+"px";
   };
   document.getElementById("start").onclick= start;
-  document.getElementById("modelsButton").onclick= showModels;
+  document.getElementById("arrangePieces").onclick = arrangePieces;
+  document.getElementById("modelsButton").onclick = showModels;
   var botoFullScreen=document.getElementById("fullscreen");
   botoFullScreen.fullScreen=false;
   botoFullScreen.onclick=function(){
@@ -336,7 +377,12 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     this.fullScreen=!this.fullScreen;
   };
+
+  // attachFastClick(document.body);
+
+  // attachFastClick(document.body);
 });
+
 function scrollBodyTop(){return document.body.scrollTop|| document.documentElement.scrollTop;}
 function final(){
   document.querySelectorAll(".move").forEach(function(e,i){
@@ -344,8 +390,8 @@ function final(){
     e.path.style.stroke="black";
   });
   document.querySelector("#modelsButton").style.height="";
-  document.querySelector("#tap").style.height="";
-  document.querySelector("#tap").style.opacity=0;
+  // document.querySelector("#tap").style.height="";
+  // document.querySelector("#tap").style.opacity=0;
   document.querySelector("#seeModel").style.top="-35px";
   document.getElementById("models").style.height=0;
   document.getElementById("time").innerHTML=timeEnd;
@@ -353,25 +399,34 @@ function final(){
 
 }
 function showModels(){
-  document.getElementById("models").style.height="2000px";
+  document.getElementById("models").style.height="calc(100vh - 100px)";
   document.getElementById("time").innerHTML="";
   document.querySelector("#modelsButton").style.height=0;
-  document.querySelector("#tap").style.height="";
-  document.querySelector("#tap").style.opacity=1;
+  // document.querySelector("#tap").style.height="";
+  document.querySelector("#tap").style.display="unset";
+  // document.querySelector("#tap").style.opacity=1;
 }
 function start(){
   document.querySelector("#start").style.height=0;
   document.querySelector("#modelsButton").style.height=0;
   document.querySelector("#seeModel").style.top="10px";
-  document.querySelector("#tap").style.height=0;
-  document.querySelector("#tap").style.opacity=0;
+  // document.querySelector("#tap").style.height=0;
+  // document.querySelector("#tap").style.opacity=0;
+  document.querySelector("#tap").style.display="none";
   document.querySelector("#modelsButton").style.height=0;
+
   document.querySelectorAll(".move").forEach(function(e,i){
+
+    let positionToSpawn = getPositionToSpawn();
+    e.style.top = positionToSpawn.top;
+    e.style.left = positionToSpawn.left;
 
     e.parentNode.removeChild(e);
     document.body.appendChild(e);
-    e.style.top= 80+ Math.random()*400 +"px";
-    e.style.left=Math.random()*(window.innerWidth -100) +"px";
+    // e.style.top= 80+ Math.random()*400 +"px";
+    // e.style.top = getRandomInt(minHeightSpawnBottom, maxHeightSpawnBottom) + "px";
+
+    // e.style.left=Math.random()*(window.innerWidth -100) +"px";
     setTimeout(function(){
       var angle=Math.floor(Math.random()*4);
       e.setAttribute("classangle","g"+angle);
@@ -380,8 +435,40 @@ function start(){
     },10);
 
   });
-  timeInitial=(new Date).getTime();
+  timeInitial = (new Date).getTime();
   document.getElementById("time").innerHTML="";
+}
+
+function arrangePieces() {
+  document.querySelectorAll("body > .move").forEach(function(e,i){
+    let positionToSpawn = getPositionToSpawn();
+    e.style.top = positionToSpawn.top;
+    e.style.left = positionToSpawn.left;
+  });
+}
+
+function getPositionToSpawn() {
+  let minHeightSpawnBottom = document.getElementById('container').getBoundingClientRect().y + document.getElementById('container').getBoundingClientRect().height;
+  let maxHeightSpawnBottom = document.body.clientHeight - sizeOfPieces;
+
+  let minSpawnLeft = 0;
+  let maxSpawnLeft = document.getElementById('container').getBoundingClientRect().x - sizeOfPieces;
+
+  let minSpawnRight = document.getElementById('container').getBoundingClientRect().x + document.getElementById('container').getBoundingClientRect().width;
+  let maxSpawnRight = window.innerWidth - sizeOfPieces ;
+
+  let sideOfSpawn = getRandomInt(0, 2);
+  //spawn bottom
+  if (sideOfSpawn === 1 || document.getElementById('container').getBoundingClientRect().x < sizeOfPieces) {
+    return {top: getRandomInt(minHeightSpawnBottom, maxHeightSpawnBottom) + "px",
+      left: getRandomInt(0, window.innerWidth - sizeOfPieces) + "px"};
+  }else if (sideOfSpawn === 0) { //spawn right
+    return {top: getRandomInt(80, window.innerHeight - sizeOfPieces) + "px",
+      left: getRandomInt(minSpawnLeft, maxSpawnLeft) + "px"};
+  } else if (sideOfSpawn === 2) { //spawn left
+    return {top: getRandomInt(80, window.innerHeight - sizeOfPieces) + "px",
+      left: getRandomInt(minSpawnRight, maxSpawnRight) + "px"};
+  }
 }
 
 function girar(){
@@ -414,6 +501,7 @@ function fixPiece(p){
   document.body.removeChild(p);
   document.querySelectorAll(".position")[p.index].appendChild(p);
   p.onmousedown="";
+  p.onclick="";
   p.onmouseover="";
   p.onmouseout="";
   p.onmousemove="";
@@ -471,13 +559,19 @@ function dropPiece(){
 document.addEventListener('mousemove', drag, true);
 document.addEventListener("touchmove", drag, true);
 
-function drag(event) {
+function drag(e) {
+  // clickCount=0;
   // event.preventDefault();
+  if (e.type === 'touchmove' ) {
+    e.clientX = e.touches[0].clientX;
+    e.clientY = e.touches[0].clientY;
+  }
+
   if (pezaSeleccionada) {
     let mousePosition = {
 
-      x : event.clientX,
-      y : event.clientY
+      x : e.clientX,
+      y : e.clientY
 
     };
     pezaSeleccionada.style.left = (mousePosition.x + offset[0]) + 'px';
@@ -489,17 +583,20 @@ function drag(event) {
 
 
 function getPos(e){
+  if (e.type === 'touchstart' ) {
+    e.clientX = e.touches[0].clientX;
+    e.clientY = e.touches[0].clientY;
+  }
 
-
-  var x=e.clientX-this.offsetLeft;
-  var y=e.clientY- this.offsetTop + scrollBodyTop();
+  let x = e.clientX - this.offsetLeft;
+  let y = e.clientY - this.offsetTop + scrollBodyTop();
 
   if ((x>0 && x<sizeOfPieces) && (y>0 && y<sizeOfPieces)){
-
-    this.onmousedown=takePiece;
-    this.touchstart=takePiece;
-    this.ondblclick=girar;
-    this.style.cursor="move";
+    takePiece.call(this, e);
+    // this.onmousedown=takePiece;
+    // this.touchstart=takePiece;
+    // this.ondblclick=girar;
+    // this.style.cursor="move";
 
     this.style.zIndex=zIndex+1;
 
@@ -507,13 +604,41 @@ function getPos(e){
 
   }else{
 
-    this.onmousedown=""; this.touchstart="";
-    this.ondblclick="";
-    this.style.cursor="default";
+    // this.onmousedown="";
+    // this.touchstart="";
+    // this.ondblclick="";
+    // this.style.cursor="default";
     this.style.zIndex=this.zIndexPrevi;
   }
 
 
+}
+
+
+let lastTarget;
+function onDblClick(e) {
+  if ( lastTarget === undefined || lastTarget !== e.target) {
+    lastTarget = e.target;
+    clickCount = 1;
+  }else {
+    clickCount++;
+  }
+
+  if (clickCount === 1) {
+    singleClickTimer = setTimeout(function() {
+      clickCount = 0;
+    }, 300);
+  } else if (clickCount === 2) {
+    clearTimeout(singleClickTimer);
+    clickCount = 0;
+    girar.call(this);
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function stopTracking(){
